26c3f907026495bcce01db97c52ce2f2f9b18d1f
John-Carlos Sanabria <johncarlos.sanabria@gmail.com>
2022-03-06 15:22:35 -0800

    "First Commit"

---
+++ JokeWebApp/Models/Joke.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace JokeWebApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string Punchline { get; set; }
+
+        public Joke()
+        {
+
+        }
+    }
+}
